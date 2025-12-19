70870c344874bb91a259ec0bdca8a4d6f4db0a68
Michael Freno <michaelt.freno@gmail.com>
2025-12-19 18:14:57 -0500

    speed-insights

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
-
+import { injectSpeedInsights } from "@vercel/speed-insights";
+injectSpeedInsights();
 mount(() => <StartClient />, document.getElementById("app")!);
