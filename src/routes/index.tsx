cc48a1382c0b3ae52cc94e20ab74dafba05defe1
Michael Freno <michaelt.freno@gmail.com>
2025-12-19 15:05:54 -0500

    cleanup for index, not good enough yet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
+import DownloadOnAppStore from "~/components/icons/DownloadOnAppStore";
 import { Typewriter } from "~/components/Typewriter";
 
 export default function Home() {
   return (
-    <main class="flex h-full flex-col p-4 text-xl">
+    <main class="flex h-full flex-col gap-8 p-4 text-xl">
       <div class="flex-1">
         <Typewriter speed={30} keepAlive={2000}>
           <div class="text-4xl">Hey!</div>
@@ -24,7 +25,6 @@ export default function Home() {
             LÖVE
           </a>{" "}
         </Typewriter>
-
         <Typewriter speed={100} keepAlive={2000}>
           You can see some of my work{" "}
           <a
@@ -34,7 +34,87 @@ export default function Home() {
             here (github)
           </a>
         </Typewriter>
+        <div class="pt-[10%] text-center">
+          Some of my recent projects:
+          <div class="flex flex-col items-center justify-center gap-4 2xl:flex-row">
+            <div class="border-surface0 flex flex-col gap-2 rounded-md border-2 p-4 text-center">
+              My mobile game:
+              <a
+                class="text-blue hover-underline-animation"
+                href="https://apps.apple.com/us/app/life-and-lineage/id6737252442"
+              >
+                Life and Lineage{" "}
+              </a>
+              <div class="flex flex-col items-center justify-center gap-2 xl:flex-row">
+                <div class="aspect-auto w-36 overflow-hidden rounded-lg md:w-52 xl:w-64">
+                  <img
+                    src="/lineage-home.png"
+                    alt="Work sample 1"
+                    class="h-full w-full object-cover"
+                  />
+                </div>
+                <div class="aspect-auto w-36 overflow-hidden rounded-lg md:w-52 xl:w-80">
+                  <video
+                    src="/lineage-preview.mp4"
+                    class="h-full w-full object-cover"
+                    autoplay
+                    loop
+                    muted
+                    playsinline
+                  />
+                </div>
+                <div class="aspect-auto w-36 overflow-hidden rounded-lg md:w-52 xl:w-64">
+                  <img
+                    src="/lineage-shops.png"
+                    alt="Work sample 2"
+                    class="h-full w-full object-cover"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="border-surface0 flex flex-col gap-2 rounded-md border-2 p-4 text-center">
+              My LÖVE UI library{" "}
+              <a
+                href="https://github.com/mikefreno/flexlove"
+                class="text-blue hover-underline-animation"
+              >
+                FlexLöve
+              </a>
+              <div class="aspect-auto w-48 overflow-hidden rounded-lg md:w-64 xl:w-80">
+                <video
+                  src="/flexlove-scrollable.mp4"
+                  class="h-full w-full object-cover"
+                  autoplay
+                  loop
+                  muted
+                  playsinline
+                />
+              </div>
+              <div class="aspect-auto w-48 overflow-hidden rounded-lg md:w-64 xl:w-80">
+                <video
+                  src="/flexlove-input.mp4"
+                  class="h-full w-full object-cover"
+                  autoplay
+                  loop
+                  muted
+                  playsinline
+                />
+              </div>
+              <div class="aspect-auto w-48 overflow-hidden rounded-lg md:w-64 xl:w-80">
+                <video
+                  src="/flexlove-slider.mp4"
+                  class="h-full w-full object-cover"
+                  autoplay
+                  loop
+                  muted
+                  playsinline
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div class="flex flex-col items-end gap-4">
         <Typewriter speed={50} keepAlive={false}>
           <div>
