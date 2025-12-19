004ab5407f2bc981a8d3e53814bc0c5cd80c8a7f
Michael Freno <michaelt.freno@gmail.com>
2025-12-19 15:43:58 -0500

    simpler

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,19 +2,11 @@ import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
 import { env } from "~/env/server";
 import { AppRouter } from "~/server/api/root";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return "";
-  // replace example.com with your actual production url
-  if (process.env.NODE_ENV === "production") return env.VITE_DOMAIN;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-};
-
-// create the client, export it
 export const api = createTRPCProxyClient<AppRouter>({
   links: [
     // will print out helpful logs when using client
     loggerLink(),
     // identifies what url will handle trpc requests
-    httpBatchLink({ url: `${getBaseUrl()}/api/trpc` })
+    httpBatchLink({ url: `${env.VITE_DOMAIN}/api/trpc` })
   ]
 });
