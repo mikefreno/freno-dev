37f6cfc811784b3920a25a11b30b0a2c601dd7bd
Michael Freno <michaelt.freno@gmail.com>
2025-12-19 15:57:42 -0500

    hardcode

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,22 @@
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client";
 import { AppRouter } from "~/server/api/root";
 
+const getBaseUrl = () => {
+  // Browser: use relative URL
+  if (typeof window !== "undefined") return "";
+
+  //const domain = import.meta.env.VITE_DOMAIN;
+  const domain = "https://freno.dev"; // try to hardcode it for now
+  if (domain) return domain;
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
 export const api = createTRPCProxyClient<AppRouter>({
   links: [
     // will print out helpful logs when using client
     loggerLink(),
     // identifies what url will handle trpc requests
-    httpBatchLink({ url: `${import.meta.env.VITE_DOMAIN}/api/trpc` })
+    httpBatchLink({ url: `${getBaseUrl()}/api/trpc` })
   ]
 });
